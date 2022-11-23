070708a184e28f1b83c398c9a1d12748cddc8f64
shohagroy <pkshohag240@gmail.com>
2022-11-23 23:43:23 +0600

    mobile mart website basic structure added

---
+++ src/Components/Pages/Login/Login.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Login = () => {
+  return <div>Login compinent</div>;
+};
+
+export default Login;
---
+++ src/Components/Pages/Signup/Signup.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Signup = () => {
+  return <div>sign uo component</div>;
+};
+
+export default Signup;
