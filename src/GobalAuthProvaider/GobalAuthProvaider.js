fc3f67332986257787a4b573025dfc46d00ab147
shohagroy <pkshohag240@gmail.com>
2022-11-24 01:41:41 +0600

    firebase app added

---
+++ src/GobalAuthProvaider/GobalAuthProvaider.js
@@ -0,0 +1,67 @@
+import React, { createContext } from "react";
+import app from "../Firebase/Firebase";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
+import { useEffect } from "react";
+import { useState } from "react";
+
+// export AuthContex = createContext
+export const AuthContex = createContext();
+const GobalAuthProvaider = ({ children }) => {
+  const [user, setUser] = useState({});
+  const [loader, setLoader] = useState(true);
+
+  const auth = getAuth(app);
+  // create user function
+  const createUser = (email, password) => {
+    setLoader(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  // update user profile
+  const updateUser = (name) => {
+    return updateProfile(auth.currentUser, { displayName: name });
+  };
+
+  // user log Out function
+  const logOut = () => {
+    setLoader(true);
+    return signOut(auth)
+      .then(() => {
+        // user sign out
+        localStorage.removeItem("token");
+        setUser({});
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+
+  // user sign in function
+  const login = (email, password) => {
+    setLoader(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+      if (currentUser) {
+        setUser(currentUser);
+      }
+      setLoader(false);
+    });
+
+    return () => unSubscribe();
+  }, []);
+
+  const authInfo = { createUser, updateUser, user, logOut, login, loader };
+  return <AuthContex.Provider value={authInfo}>{children}</AuthContex.Provider>;
+};
+
+export default GobalAuthProvaider;
