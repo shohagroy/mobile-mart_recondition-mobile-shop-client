c1c11de09909543f19bd596e5d0fc8fbde579dbb
shohagroy <pkshohag240@gmail.com>
2022-11-24 10:12:48 +0600

    my product page added

--- src/Components/Pages/Dashbord/MyProduct/MyProduct.js
+++ src/Components/Pages/Dashbord/MyProduct/MyProduct.js
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import { AuthContex } from "../../../../GobalAuthProvaider/GobalAuthProvaider";
 import { useQuery } from "@tanstack/react-query";
 import LoadingLoader from "../../../Shared/Loader/LoadingLoader";
+import { Link } from "react-router-dom";
 
 const MyProduct = () => {
   const { user } = useContext(AuthContex);
@@ -29,14 +30,69 @@ const MyProduct = () => {
   if (isLoading) {
     return <LoadingLoader />;
   }
-  console.log(products);
+
+  const removeProductHandelar = (id) => {
+    console.log(id);
+  };
 
   return (
     <section className="w-full">
       <h3 className="text-2xl text-center  md:text-left mx-4  font-semibold text-accent">
-        Add Product -
+        My Product -<strong className="text-primary">{products?.length}</strong>
       </h3>
-      <div className="w-full flex justify-center items-center"></div>
+
+      <div className="overflow-x-auto my-10">
+        <table className="table w-full">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Images</th>
+              <th>Name</th>
+              <th>Price</th>
+              <th>Advertise</th>
+              <th>Status</th>
+              <th>Deleted</th>
+            </tr>
+          </thead>
+          <tbody>
+            {products?.map((product, i) => (
+              <tr key={i}>
+                <th>{i + 1}</th>
+                <td>
+                  <div className="avatar">
+                    <div className="w-28 mask mask-squircle">
+                      <img
+                        src="https://placeimg.com/192/192/people"
+                        alt={product?.productName}
+                      />
+                    </div>
+                  </div>
+                </td>
+                <td>
+                  <Link className="hover:text-primary">
+                    {product?.productName}
+                  </Link>
+                </td>
+                <td className="text-primary font-semibold">
+                  ${product?.sellPrice}
+                </td>
+                <td>
+                  <button className="btn btn-sm">Advertise</button>
+                </td>
+                <td className="text-primary font-bold">Unsole</td>
+                <td>
+                  <button
+                    onClick={() => removeProductHandelar(product._id)}
+                    className="btn btn-sm bg-red-600 text-white"
+                  >
+                    Remove
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </section>
   );
 };
